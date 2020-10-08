9d9183a434965bc8cb513af3de7ca0712fa056df
sashitaghimire <ghimiresashita@gmail.com>
2020-10-08 19:50:14 +0545

    project completed

--- src/context.js
+++ src/context.js
@@ -80,17 +80,92 @@ getItem = (id)=>{
     };
 
     increment = (id) =>{
-        console.log("increment method")
+       let tempCart = [...this.state.cart];
+
+       const selectedProduct = tempCart.find(item=>item.id === id);
+
+       const index = tempCart.indexOf(selectedProduct);
+
+       const product = tempCart[index];
+
+       product.count = product.count + 1;
+       product.total = product.count * product.price;
+
+       this.setState(()=>{
+           return {cart:[...tempCart]}
+
+       },()=>{
+           this.addTotals();
+
+       }
+       )
+
+
     };
     decrement = (id) =>{
-        console.log("increment method")
+        let tempCart = [...this.state.cart];
+
+       const selectedProduct = tempCart.find(item=>item.id === id);
+
+       const index = tempCart.indexOf(selectedProduct);
+
+       const product = tempCart[index];
+
+       product.count = product.count - 1 ;
+
+       if(product.count === 0){
+           this.removeItem(id);
+       }
+       else{
+           product.total=product.count * product.price;
+           this.setState(()=>{
+            return {cart:[...tempCart]}
+    
+        },()=>{
+            this.addTotals();
+    
+        }
+        )
+       }
+       
     };
 
     removeItem = (id) =>{
-        console.log('item removed');
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart= false;
+        removedProduct.count= 0;
+        removedProduct.total= 0;
+
+
+        this.setState(()=>{
+            return{
+                cart:[...tempCart],
+                products: [...tempProducts]
+            }
+        },
+        ()=>{
+            this.addTotals();
+        }
+        
+        )
+
+
     };
     clearCart = () =>{
-        console.log("cart was cleared")
+        this.setState(()=>{
+            return{cart:[]};
+        },()=>{
+            this.storeProducts();
+            this.addTotals();
+        }
+        )
     };
 
     addTotals = () =>{
